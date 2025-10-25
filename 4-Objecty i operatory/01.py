32685057d7e00425707a57ec15f9240aeb0801c0
cherrydan <cherrydan.1978@gmail.com>
2025-10-25 13:44:49 +0300

    Continue Chapter 4...

---
+++ 4-Objecty i operatory/01.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+DISTANCE = 1422
+SPEED = 72
+
+print('Сундар при средней скорости {} км/ч достигнет Дели, расстояние'
+      .format(SPEED) + 
+      ' до которого {} км за {} часов'.format(DISTANCE, DISTANCE // SPEED))
