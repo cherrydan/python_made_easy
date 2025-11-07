b3cb195ee5782592cfcc3cce7e1d31e55ab4f124
cherrydan <cherrydan.1978@gmail.com>
2025-11-07 18:34:04 +0300

    Exercise #9

--- 4-Objecty i operatory/test/testcube.py
+++ 4-Objecty i operatory/test/testcube.py
@@ -11,3 +11,8 @@ def test_capacity(arg, expected_res):
 @pytest.mark.parametrize("arg, expected_res", [(2.23, 9.9458), (5, 50.00), (10, 200)])
 def test_square(arg, expected_res):
     assert cubefunc.cube_square(arg) == expected_res
+
+
+@pytest.mark.parametrize("arg1, arg2, arg3, expected_res", [(3, 4, 5, 6), (2, 3, 4, 5)])
+def test_newcube(arg1, arg2, arg3, expected_res):
+    assert cubefunc.new_cube(arg1, arg2, arg3) == expected_res
