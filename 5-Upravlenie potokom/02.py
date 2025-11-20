1601c19502a55e00600b10feca56cbea5d512ffd
cherrydan <cherrydan.1978@gmail.com>
2025-11-20 19:17:09 +0300

    add error handling to ex. 02 and 03

--- 5-Upravlenie potokom/02.py
+++ 5-Upravlenie potokom/02.py
@@ -11,16 +11,24 @@ employees = {
     'Mohit': 0
 }
 
-for k, v in employees.items():
-    employees[k] = float(input(f'Input expirience for {k} '))
+try:
+    for k, v in employees.items():
+        employees[k] = float(input(f'Input expirience for {k} '))
 
 
 
-overexperienced = max(employees.values())
-key = next((key for key, value in employees.items() if value == overexperienced), None)
-print(f'Most experienced is {key}')
+    overexperienced = max(employees.values())
+    key = next((key for key, value in employees.items() if value == overexperienced), None)
+    print(f'Most experienced is {key}')
 
-underexpierenced = min(employees.values())
-key = next((key for key, value in employees.items() if value == underexpierenced), None)
-print(f'Most UNexpirienced is {key}')
+    underexpierenced = min(employees.values())
+    key = next((key for key, value in employees.items() if value == underexpierenced), None)
+    print(f'Most UNexpirienced is {key}')
+
+except ValueError:
+    print('Only digits are allowed!')
+    exit(-1)
+
+finally:
+    print('Program terminated successfully')
 
